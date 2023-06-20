08f891b0f108db5d9c142e6140c5f8118c7416da
Yannick fumukani <slashcs7@hotmail.com>
2023-06-19 23:23:26 -0500

    Application config implemented

--- src/main/java/com/slashfmk/ecommerce/config/JwtAuthenticationFilter.java
+++ src/main/java/com/slashfmk/ecommerce/config/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,6 +20,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -39,5 +43,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
         // TODO extract the username from jwt token
         userName = jwtService.extractUsername(jwt);
+
+        if(userName == null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadByUsername(userName);
+        }
+
     }
 }
